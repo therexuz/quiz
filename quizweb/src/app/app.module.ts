1a6efc8c953f365fd262acf3a9f4a8fa0a9dd750
therexuz <ignavaldecace98@gmail.com>
2021-04-28 04:35:24 -0400

    Reinicio

---
+++ quizweb/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { CardViewComponent } from './card-view/card-view.component';
+import { CreateClientComponent } from './create-client/create-client.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ListViewComponent,
+    CardViewComponent,
+    CreateClientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
