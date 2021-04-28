6ffb8640db91c7bfff8be8623a9c5f216a8021cf
therexuz <ignavaldecace98@gmail.com>
2021-04-28 05:27:32 -0400

    Preparar servicio

--- quizweb/src/app/app-routing.module.ts
+++ quizweb/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardViewComponent } from './card-view/card-view.component';
 import { CreateClientComponent } from './create-client/create-client.component';
 import { ListViewComponent } from './list-view/list-view.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 
 const routes: Routes = [
   {path:'card-view',component:CardViewComponent},
